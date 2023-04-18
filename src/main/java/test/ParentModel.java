9c2611632dd07fbca08eebdeb4d027c55315c320
rebekahoj <olojadeborah37@gmail.com>
2023-04-18 13:34:59 +0100

    initial commit

--- src/main/java/test/ParentModel.java
+++ src/main/java/test/ParentModel.java
@@ -3,51 +3,69 @@ package test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Parent {
-    WebDriver driver = new ChromeDriver();
+public class ParentModel {
+    private final WebDriver driver;
+    private final By email = By.id("email");
+    private final By password = By.id("password");
+    private final By submit = By.id("submit");
+    private final By registerButton = By.id("register-parent");
+    private final By firstNameInput = By.id("firstName");
+    private final By lastName = By.id("lastName");
+    private final By emailAddress = By.id("email");
+    private final By phoneNumber = By.id("phoneNumber");
+    private final By select = By.id("select-field");
+    private final By role = By.id("mother");
+    private final By createAccount = By.id("submit");
 
-    public static void main(String[] args) {
-
+    private final By success = By.id("success");
+    public ParentModel(WebDriver driver) {
+        this.driver = driver;
     }
 
-    public void login() {
-
-        driver.get("https://secure-school-admin.netlify.app/school/parent");
-
-        WebElement email = driver.findElement(By.id("email"));
-        WebElement password = driver.findElement(By.id("password"));
-        WebElement submit = driver.findElement(By.id("submit"));
-
-        email.sendKeys("olojadeborah37@gmail.com");
-        password.sendKeys("Lumaterm1");
-        submit.click();
+    public void enterEmail(String emailLogin){
+        driver.findElement(email).sendKeys(emailLogin);
     }
 
-    public void createNewParent(){
-
-        driver.navigate().to("https://secure-school-admin.netlify.app/school/parent");
-        
-        WebElement registerButton = driver.findElement(By.id("register-parent"));
-        WebElement uploadImage = driver.findElement(By.id("polaroid"));
-        WebElement firstName = driver.findElement(By.id("firstName"));
-        WebElement lastName = driver.findElement(By.id("lastName"));
-        WebElement emailAddress = driver.findElement(By.id("email"));
-        WebElement phoneNumber = driver.findElement(By.id("phoneNumber"));
-        WebElement select = driver.findElement(By.id("select-field"));
-        WebElement role = driver.findElement(By.cssSelector("option[value*='Mother']"));
-        WebElement createAccount = driver.findElement(By.id("submit"));
-
-
-        registerButton.click();
-        uploadImage.sendKeys("IMG_4662.PNG");
-        firstName.sendKeys("Obakam");
-        lastName.sendKeys("Tom");
-        emailAddress.sendKeys("obakamtom@gmail.com");
-        phoneNumber.sendKeys("08123456789");
-        select.click();
-        role.click();
-        createAccount.click();
+    public void enterPassword(String password1){
+        driver.findElement(password).sendKeys(password1);
     }
+
+    public void clickLoginButton(){
+        driver.findElement(submit).click();
+    }
+
+    //parent creation
+    public void clickRegisterButton() {
+        driver.findElement(registerButton).click();
+    }
+
+    public void enterFirstName(String firstname) {
+        driver.findElement(firstNameInput).sendKeys(firstname);
+    }
+
+    public void enterLastName(String surname) {
+        driver.findElement(lastName).sendKeys(surname);
+    }
+
+    public void enterEmailAddress(String email) {
+        driver.findElement(emailAddress).sendKeys(email);
+    }
+
+    public void enterPhoneNumber(String phonenumber) {
+       driver.findElement(phoneNumber).sendKeys(phonenumber);
+    }
+
+    public void clickSelect() {
+        driver.findElement(select).click();
+    }
+
+    public void selectRole() {
+        driver.findElement(role).click();
+    }
+
+    public void createAccount() {
+       driver.findElement(createAccount).click();
+    }
+
 }
--- src/test/java/Parent.java
+++ src/test/java/Parent.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class Parent {
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import test.ParentModel;
+
+import java.time.Duration;
+
+public class Parent {
+
+       private WebDriver driver;
+       private ParentModel parentModel;
+
+       private WebDriverWait wait;
+
+       @BeforeTest
+       public void setUp(){
+
+      driver = new ChromeDriver();
+
+      driver.get("https://secure-school-admin.netlify.app/school/parent");
+
+      parentModel = new ParentModel(driver);
+
+
+       }
+
+       @Test
+       public void loginTest(){
+           parentModel.enterEmail("Olojadeborah37@gmail.com");
+           parentModel.enterPassword("Lumaterm1");
+           parentModel.clickLoginButton();
+       }
+
+       @Test
+       public void parentCreationTest(){
+
+           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+           parentModel.clickRegisterButton();
+           parentModel.enterFirstName("Zed");
+           parentModel.enterLastName("tolu");
+           parentModel.enterEmailAddress("zed@gmail.com");
+           parentModel.enterPhoneNumber("08067894578");
+           parentModel.clickSelect();
+           parentModel.selectRole();
+           parentModel.createAccount();
+
+           WebElement login = driver.findElement(By.id("login-success"));
+           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+           Assert.assertTrue(login.isDisplayed());
+
+           //String actualMessage = "login successful";
+           //String expectedMessage = "login successful";
+
+           //Assert.assertEquals(actualMessage, expectedMessage);
+
+           WebElement success = driver.findElement(By.id("success"));
+           driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
+           success.getText();
+
+    }
 }
